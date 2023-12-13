947ab3981e45c0bfafb589bbac69b6aaa56a6530
chappy <lumangyao112@gmail.com>
2023-12-14 06:41:06 +0800

    fix

--- src/pages/owner/dashboard/components/CardOptions/index.tsx
+++ src/pages/owner/dashboard/components/CardOptions/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
+import useAlertOption from '../../../../../hooks/useAlertOption';
+import Swal from 'sweetalert2';
+import { Routes } from '../../../../../types/Routes.enum';
 
 type Props = {
     icon:any;
     title:string;
     redirect:string;
+    subscription:any;
 }
 
 export default function CardOptions(props:Props) {
-  
+    const {alertError} = useAlertOption();
+
     function handleRedirect(){
+        if(!props.subscription){
+           Swal.fire({
+            icon:'warning',
+            text:'You have no subscription'
+           }).then(res=>{
+            if(res.isConfirmed){
+                window.location.href=Routes.SUBSCRIPTION
+            }
+           });
+           return;
+        }
+
+        if(props.subscription.usersub_status === 'EXPIRED'){
+            Swal.fire({
+                icon:'warning',
+                text:'Youre subscription is expired'
+               }).then(res=>{
+                if(res.isConfirmed){
+                    window.location.href=Routes.SUBSCRIPTION
+                }
+               });
+               return;
+        }
         window.location.href=props.redirect
     }
   
--- src/pages/owner/dashboard/index.tsx
+++ src/pages/owner/dashboard/index.tsx
@@ -1,14 +1,16 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Container } from '../../../component';
 import useGetAccountFromStorage from '../../../hooks/useGetAccountFromStorage';
 import { formatFullName } from '../../../utils/string';
 import CardOptions from './components/CardOptions';
 import { Routes } from '../../../types/Routes.enum';
 import TransactionTabs from '../../../component/TransactionTabs';
+import useAlertOption from '../../../hooks/useAlertOption';
+import { getUserSubscription } from '../../../services/UserSubscription.service';
+import { getDataFromStorage } from '../../../utils/storage';
 
 const DRIVER = require('../../../assets/option/driver.png');
 const TRANSACTIONS = require('../../../assets/option/transaction.png');
-const BOOKINGS = require('../../../assets/option/booking.png');
 const VEHICLE = require('../../../assets/option/vehicle.png');
 
 const OPTION = [
@@ -37,18 +39,55 @@ const OPTION = [
 
 export default function DashBoard() {
   const {user} = useGetAccountFromStorage();
-  
+  const [mySubscription,setMySubscription] = useState<any>(null);
+  const {alertError,alertSuccess} = useAlertOption();
+
   const displayOption = useMemo(() => {
     return OPTION.map((val)=>(
-      <CardOptions icon={val.icon} title={val.title} redirect={val.redirect}/>
+      <CardOptions icon={val.icon} title={val.title} redirect={val.redirect} subscription={mySubscription}/>
     ))
-  }, [OPTION])
+  }, [mySubscription])
+  
+  const sendRequest = useCallback(async()=>{
+    try {
+      const user = await getDataFromStorage('account');
+      if(!user){
+        return;
+      }
+      const resp = await getUserSubscription(user?.user_id);
+
+      setMySubscription(resp.data);
+    } catch (error) {
+      alertError();
+    }
+  },[])
+
+  useEffect(() => {
+    sendRequest();
+  }, [])
+
+  const displaySubscription = useMemo(()=>{
+    if(!mySubscription){
+      return <p className=' font-bold text-white'>No Subscription</p>
+    }
+
+    return <div className=' mt-5'>
+      <p className=' text-bold text-lg font-bold text-white'>{mySubscription.sub_name}</p> 
+      <p className=' text-bold text-md text-white'>{mySubscription.sub_description}</p>
+      <div className=' h-5'/>
+      <p className=' text-bold text-md text-white'>Validity until {mySubscription.validity}</p>
+      {mySubscription.usersub_status === 'EXPIRED' && 
+        <p className=' text-red-500 font-bold'>Expired</p>
+      }
+    </div>
+  },[mySubscription])
   
   return (
     <Container>
        <div className=' flex  w-full items-center flex-col justify-center'>
           <div className=' w-1/2 bg-slate-900  p-10'>
             <h1 className=' text-white font-bold text-xl'>Welcome, {formatFullName({firstName:user?.firstname,middleName:user?.middlename,lastName:user?.lastname})}</h1>
+            {displaySubscription}
           </div>
           <div className=' h-10'/>
           <div className=' w-1/2 grid grid-cols-2 gap-3'>
